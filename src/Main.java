5bdd3a5428254ff73b2a9f875590fb1cb3f3a854
aktov <aktovi@gmail.com>
2022-04-17 12:54:24 +0300

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Задание 1
+        String firstName = "Иванов" ;
+        String middleName = "Семен";
+        String lastName = "Семёнович";
+        String fullName = firstName + ' ' + middleName + ' ' + lastName;
+        System.out.println(fullName);
+
+        //Задание 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        //Задание 3
+        String correctFullName = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника — " + correctFullName);
     }
 }
